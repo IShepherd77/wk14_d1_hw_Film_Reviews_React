f154cf37498d2172b82d299a53276697546f3b09
Ian Shepherd <ianshepherd77@gmail.com>
2019-10-28 17:41:52 +0000

    added Film Container file

---
+++ src/containers/FilmContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FilmContainer extends Component {
+    render() {
+        return (
+            <div className="film-container">
+                Film Container here!
+            </div>
+
+        )
+    }
+}
+
+export default FilmContainer;
