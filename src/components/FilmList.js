5eca25e91f48eb534902b62fda2a0de4ea20b350
Ian Shepherd <ianshepherd77@gmail.com>
2019-10-28 17:51:46 +0000

    added Film List file

---
+++ src/components/FilmList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class FilmList extends Component {
+    render() {
+        return(
+            <div className="film-list">
+                Film List goes here!
+            </div>
+        )
+    }
+}
+
+export default FilmList;
--- src/containers/FilmContainer.js
+++ src/containers/FilmContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import FilmList from '../components/FilmList';
 
 class FilmContainer extends Component {
     render() {
         return (
             <div className="film-container">
-                Film Container here!
+                <h2>Upcoming Film Releases for UK</h2>
+                <FilmList />
             </div>
 
         )
