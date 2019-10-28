172da0b0e54db5c08f4574257a30e4019c1df984
Ian Shepherd <ianshepherd77@gmail.com>
2019-10-28 18:00:06 +0000

    added Film file

---
+++ src/components/Film.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Film extends Component {
+    render() {
+        return(
+            <div className="film">
+                Film here!
+            </div>
+        )
+    }
+}
+
+export default Film;
--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Film from './Film';
 
 class FilmList extends Component {
     render() {
         return(
             <div className="film-list">
-                Film List goes here!
+                <p>Film List goes here!</p>
+                <p>Film List goes here!</p>
             </div>
         )
     }
